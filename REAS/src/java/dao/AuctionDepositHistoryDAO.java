0c6b664113332617b9f6cbcb5e204395a2203d8c
Quyền Lê <daiquyen0@gmail.com>
2024-03-28 22:43:10 +0700

    Done danh sách refund tiền

--- REAS/src/java/dao/AuctionDepositHistoryDAO.java
+++ REAS/src/java/dao/AuctionDepositHistoryDAO.java
@@ -6,6 +6,8 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import mylib.DBUtils;
 
@@ -35,12 +37,8 @@ public class AuctionDepositHistoryDAO {
         long a = 0;
         Connection cn = DBUtils.getConnection();
         if (cn != null) {
-            String sql = "SELECT SUM(Unique_count) AS STPTD\n"
-                    + "FROM (\n"
-                    + "    SELECT [AuctionID], COUNT(DISTINCT [WalletID]) AS Unique_count\n"
-                    + "    FROM AuctionDepositHistory\n"
-                    + "    GROUP BY [AuctionID]\n"
-                    + ") AS subquery;";
+            String sql = "SELECT   COUNT ( AuctionDepositHisID) AS STPTD\n"
+                    + "FROM AuctionDepositHistory";
             PreparedStatement pst = cn.prepareStatement(sql);
             ResultSet rs = pst.executeQuery();
             if (rs != null) {
@@ -126,12 +124,8 @@ public class AuctionDepositHistoryDAO {
         long a = 0;
         Connection cn = DBUtils.getConnection();
         if (cn != null) {
-            String sql = "SELECT SUM(Unique_count) AS STPTD\n"
-                    + "FROM (\n"
-                    + "    SELECT [AuctionID], COUNT(DISTINCT [WalletID]) AS Unique_count \n"
-                    + "    FROM AuctionDepositHistory WHERE MONTH(DateAndTime) = ?\n"
-                    + "    GROUP BY [AuctionID]\n"
-                    + ") AS subquery;";
+            String sql = "SELECT   COUNT ( AuctionDepositHisID) AS STPTD\n"
+                    + "FROM AuctionDepositHistory WHERE MONTH(DateAndTime) = ?\n";
             PreparedStatement pst = cn.prepareStatement(sql);
             pst.setString(1, month);
             ResultSet rs = pst.executeQuery();
@@ -195,11 +189,79 @@ public class AuctionDepositHistoryDAO {
         return a;
     }
 
-    public static void main(String[] args) throws SQLException, ClassNotFoundException {
-        ArrayList<AuctionDepositHistory> getListADH = getListADH();
-        for (AuctionDepositHistory auctionDepositHistory : getListADH) {
-            System.out.println(auctionDepositHistory.getAuctionID() + "/" + auctionDepositHistory.getQuantity());
-        }
+    public static ArrayList<AuctionDepositHistory> getListRefund(String AuctionID) throws SQLException, ClassNotFoundException {
+        ArrayList<AuctionDepositHistory> a = new ArrayList<>();
 
+        Connection cn = DBUtils.getConnection();
+        if (cn != null) {
+            String sql = "SELECT WalletID, DateAndTime, Quantity\n"
+                    + "FROM AuctionDepositHistory\n"
+                    + "WHERE AuctionID = ?\n"
+                    + "ORDER BY DateAndTime ASC\n"
+                    + "OFFSET 0 ROWS\n"
+                    + "FETCH NEXT (SELECT COUNT(*) - 1 FROM AuctionDepositHistory WHERE AuctionID LIKE '%3%') ROWS ONLY;";
+            PreparedStatement pst = cn.prepareStatement(sql);
+            pst.setString(1, AuctionID);
+            ResultSet rs = pst.executeQuery();
+            if (rs != null) {
+                while (rs.next()) {
+                    AuctionDepositHistory adh = new AuctionDepositHistory();
+                    String walletID = rs.getString("WalletID");
+                    long quantity = rs.getLong("Quantity");
+                    Timestamp time = rs.getTimestamp("DateAndTime");
+                    LocalDateTime DateAndTime = time.toLocalDateTime();
+                    adh.setWalletID(walletID);
+                    adh.setDateAndTime(DateAndTime);
+                    adh.setQuantity(quantity);
+
+                    a.add(adh);
+                }
+            }
+            cn.close();
+        }
+        return a;
+    }
+
+    public static ArrayList<AuctionDepositHistory> getListRefundDetail(String AuctionID) throws SQLException, ClassNotFoundException {
+        ArrayList<AuctionDepositHistory> a = new ArrayList<>();
+
+        Connection cn = DBUtils.getConnection();
+        if (cn != null) {
+            String sql = "SELECT WalletID, SUM(Quantity) AS TotalQuantity\n"
+                    + "FROM (\n"
+                    + "    SELECT WalletID, Quantity, ROW_NUMBER() OVER (ORDER BY DateAndTime ASC) AS RowNum\n"
+                    + "    FROM AuctionDepositHistory\n"
+                    + "    WHERE AuctionID = ?\n"
+                    + ") AS Subquery\n"
+                    + "WHERE RowNum < (SELECT COUNT(*) FROM AuctionDepositHistory WHERE AuctionID LIKE '%3%')\n"
+                    + "GROUP BY WalletID;";
+            PreparedStatement pst = cn.prepareStatement(sql);
+            pst.setString(1, AuctionID);
+            ResultSet rs = pst.executeQuery();
+            if (rs != null) {
+                while (rs.next()) {
+                    AuctionDepositHistory adh = new AuctionDepositHistory();
+                    String walletID = rs.getString("WalletID");
+                    long quantity = rs.getLong("TotalQuantity");
+                    
+                    adh.setWalletID(walletID);
+                    
+                    adh.setQuantity(quantity);
+
+                    a.add(adh);
+                }
+            }
+            cn.close();
+        }
+        return a;
+    }
+
+    public static void main(String[] args) throws SQLException, ClassNotFoundException {
+        ArrayList<AuctionDepositHistory> getListADH = getListRefundDetail("M3");
+        for (AuctionDepositHistory auctionDepositHistory : getListADH) {
+            System.out.println(auctionDepositHistory.getWalletID() +"|" + auctionDepositHistory.getQuantity());
+        }
+//        Long a = getSoNgThamGiaTrongTungCuocDauGiaInMonth("3");
+//        System.out.println(a);
     }
 }
